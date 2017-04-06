599a59c7611776cb388aa68693ee88ba4ccefa85
stefano <stefano.siano91@gmail.com>
2017-04-06 17:44:08 +0200

    added method to get bitmap from drawable

--- powerlessimageview/src/main/java/com/stefanosiano/powerlessimageview/shape/drawers/ShapeDrawerManager.java
+++ powerlessimageview/src/main/java/com/stefanosiano/powerlessimageview/shape/drawers/ShapeDrawerManager.java
@@ -1,10 +1,15 @@
 package com.stefanosiano.powerlessimageview.shape.drawers;
 
+import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.Log;
 
 /**
@@ -16,10 +21,42 @@ public class ShapeDrawerManager {
     Paint paint;
 
     public ShapeDrawerManager() {
-        shader = new BitmapShader(null, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        shader = new BitmapShader(getBitmapFromDrawable(null), Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
         paint = new Paint();
         paint.setShader(shader);
     }
+
+
+    private Bitmap getBitmapFromDrawable(Drawable drawable) {
+        if (drawable == null) {
+            return null;
+        }
+
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        try {
+            Bitmap bitmap;
+
+            if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+                bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+            } else if (drawable instanceof ColorDrawable) {
+                bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+            } else {
+                bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            }
+
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
 
     /** Draws the image through the progress indicator */
